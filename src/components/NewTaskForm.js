d2c4d27f3efb6bc9ae1dca945669af79cbe05caa
JosephKagwi <josephkagwi28@gmail.com>
2023-11-06 11:21:05 +0300

    Assignment Complete

--- src/components/NewTaskForm.js
+++ src/components/NewTaskForm.js
@@ -1,19 +1,39 @@
-import React from "react";
+import React, { useState } from "react";
 
-function NewTaskForm() {
+function NewTaskForm(props) {
+  const { categories } = props;
+  const [text, setText] = useState("");
+  const [category, setCategory] = useState("");
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    const newTask = {
+      id: Math.random(),
+      text: text,
+      category: category,
+    };
+    props.onTaskFormSubmit(newTask);
+    setText("");
+    setCategory("");
+  };
+
   return (
-    <form className="new-task-form">
-      <label>
-        Details
-        <input type="text" name="text" />
-      </label>
-      <label>
-        Category
-        <select name="category">
-          {/* render <option> elements for each category here */}
-        </select>
-      </label>
-      <input type="submit" value="Add task" />
+    <form className="new-task-form" onSubmit={handleSubmit}>
+      <input
+        type="text"
+        placeholder="Task description"
+        value={text}
+        onChange={(e) => setText(e.target.value)}
+      />
+      <select value={category} onChange={(e) => setCategory(e.target.value)}>
+        <option value="">Select a category</option>
+        {categories.map((category) => (
+          <option key={category} value={category}>
+            {category}
+          </option>
+        ))}
+      </select>
+      <button type="submit">Add Task</button>
     </form>
   );
 }
--- src/components/Task.js
+++ src/components/Task.js
@@ -1,11 +1,19 @@
 import React from "react";
 
-function Task() {
+function Task(props) {
+  const { task, deleteTask } = props;
+
+  const handleDelete = () => {
+    deleteTask(task.id);
+  };
+
   return (
     <div className="task">
-      <div className="label">CATEGORY HERE</div>
-      <div className="text">TEXT HERE</div>
-      <button className="delete">X</button>
+      <div className="label">{task.category}</div>
+      <div className="text">{task.text}</div>
+      <button className="delete" onClick={handleDelete}>
+        Delete
+      </button>
     </div>
   );
 }
